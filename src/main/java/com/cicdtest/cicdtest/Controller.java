48f5542ef85585279d8623b20742ff339e8e2c38
1arged <94172354+1argeD@users.noreply.github.com>
2024-01-14 17:36:34 +0900

    Initial commit

---
+++ src/main/java/com/cicdtest/cicdtest/Controller.java
@@ -0,0 +1,13 @@
+package com.example.cicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("test")
+    public String test() {
+        return "해당 문구를 출력합니다.";
+    }
+}
