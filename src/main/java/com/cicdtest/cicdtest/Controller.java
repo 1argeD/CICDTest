0c5b3779f98695387a3ad4b1e59e676ac1efb8cb
1arged <94172354+1argeD@users.noreply.github.com>
2024-01-15 00:37:50 +0900

    <chore>주소변경

--- src/main/java/com/cicdtest/cicdtest/Controller.java
+++ src/main/java/com/cicdtest/cicdtest/Controller.java
@@ -1,12 +1,11 @@
 package com.cicdtest.cicdtest;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @GetMapping("cicdTest")
+    @GetMapping("board/cicdTest")
     public String test() {
         return "index1";
     }
