a37dcef4f4c2e61927796a1f4016dd0dfd36f94f
1arged <94172354+1argeD@users.noreply.github.com>
2024-01-15 03:55:20 +0900

    <feat>postmapping 추가

--- src/main/java/com/cicdtest/cicdtest/controller/Controller.java
+++ src/main/java/com/cicdtest/cicdtest/controller/Controller.java
@@ -2,6 +2,7 @@ package com.cicdtest.cicdtest.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -11,5 +12,9 @@ public class Controller {
     @GetMapping("/cicd")
     public ResponseEntity<?> test() {
         return ResponseEntity.ok().body(Map.of("msg","재배포 된거임???"));
+    }
+    @PostMapping("mapping")
+    public ResponseEntity<?> edit() {
+        return ResponseEntity.ok().body(Map.of("msg","포스트멥핑 추가하기"));
     }
 }
